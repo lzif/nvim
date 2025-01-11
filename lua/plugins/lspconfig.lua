a03b4df06f1d9bdcb5e3845b27ba4c5ba62a9a8a
lzif <lukyfriendly@gmail.com>
2025-01-11 07:43:31 +0700

    feat(lazy.lua): disable lazy plugin checker and change detection to improve startup time

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -43,6 +43,9 @@ return {
 	  local lspconfig = require('lspconfig')
 	  local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
 
+		lspconfig.rust_analyzer.setup({
+			capabilities = lsp_capabilities
+		})
 	  require('mason-lspconfig').setup({
 	    ensure_installed = {
 	      'ts_ls',
